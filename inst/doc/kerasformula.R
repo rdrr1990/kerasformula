858f317b74b47f600c013f6e2ade4a67b76772d8
Pete Mohanty <pete.mohanty@gmail.com>
2018-01-03 14:07:28 -0800

    eval=FALSE for Rmds

--- inst/doc/kerasformula.R
+++ inst/doc/kerasformula.R
@@ -2,25 +2,37 @@
 library(knitr)
 opts_chunk$set(comment = "", message = FALSE, warning = FALSE)
 
-## ------------------------------------------------------------------------
-library(kerasformula)
-max_features <- 5000 # 5,000 words (ranked by popularity) found in movie reviews
-maxlen <- 50  # Cut texts after 50 words (among top max_features most common words) 
-Nsample <- 1000 
+## ---- eval = FALSE-------------------------------------------------------
+#  install.packages("keras")
+#  library(keras)
+#  install_keras()
 
-cat('Loading data...\n')
-imdb <- keras::dataset_imdb(num_words = max_features)
-imdb_df <- as.data.frame(cbind(c(imdb$train$y, imdb$test$y),
-                               pad_sequences(c(imdb$train$x, imdb$test$x))))
-set.seed(2017)
-demo_sample <- sample(nrow(imdb_df), Nsample)
-P <- ncol(imdb_df) - 1
-colnames(imdb_df) <- c("y", paste0("x", 1:P))
+## ---- eval = FALSE-------------------------------------------------------
+#  library(kerasformula)
+#  max_features <- 5000 # 5,000 words (ranked by popularity) found in movie reviews
+#  maxlen <- 50  # Cut texts after 50 words (among top max_features most common words)
+#  Nsample <- 1000
+#  
+#  cat('Loading data...\n')
+#  imdb <- keras::dataset_imdb(num_words = max_features)
+#  imdb_df <- as.data.frame(cbind(c(imdb$train$y, imdb$test$y),
+#                                 pad_sequences(c(imdb$train$x, imdb$test$x))))
+#  
+#  set.seed(2017)   # can also set kms(..., seed = 2017)
+#  
+#  demo_sample <- sample(nrow(imdb_df), Nsample)
+#  P <- ncol(imdb_df) - 1
+#  colnames(imdb_df) <- c("y", paste0("x", 1:P))
+#  
+#  out_dense <- kms("y ~ .", data = imdb_df[demo_sample, ], Nepochs = 10)
+#  
+#  plot(out_dense$history)  # incredibly useful
+#  # choose Nepochs to maximize out of sample accuracy
+#  
+#  out_dense$confusion
 
-out_dense <- kms("y ~ .", data = imdb_df[demo_sample, ], Nepochs = 10)
-plot(out_dense$history)
-out_dense$confusion
-cat('Test accuracy:', out_dense$evaluations$acc, "\n")
+## ---- eval=FALSE---------------------------------------------------------
+#  cat('Test accuracy:', out_dense$evaluations$acc, "\n")
 
 ## ------------------------------------------------------------------------
 out_dense <- kms("y ~ .", data = imdb_df[demo_sample, ], Nepochs = 10, 
@@ -28,9 +40,11 @@ out_dense <- kms("y ~ .", data = imdb_df[demo_sample, ], Nepochs = 10,
                                activation = c("relu", "relu", "relu", "softmax"), 
                                dropout = c(0.5, 0.4, 0.3, NA)))
 out_dense$confusion
-cat('Test accuracy:', out_dense$evaluations$acc, "\n")
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval = FALSE-------------------------------------------------------
+#  cat('Test accuracy:', out_dense$evaluations$acc, "\n")
+
+## ---- eval = FALSE-------------------------------------------------------
 #  k <- keras_model_sequential()
 #  k %>%
 #    layer_embedding(input_dim = max_features, output_dim = 128) %>%
